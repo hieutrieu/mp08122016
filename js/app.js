f7e2c6346cd534ef02d2c3eed1819d780ff55ff0
hieutrieu <mrhieutrieu@gmail.com>
2016-01-05 16:23:45 +0700

    a

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
             ($(window).scrollTop() > navHeight) ? $('#navbar-top-menu').addClass('navbar-fixed-top') : $('#navbar-top-menu').removeClass('navbar-fixed-top');
             $('#navbar-top-menu').css('display: block; transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)');
         });
-        if($('#carousel').length > 0) {
+        if ($('#carousel').length > 0) {
             $('#carousel').carouFredSel({
                 prev: {
                     button: ".prev",
@@ -24,20 +24,21 @@
             });
         }
         handleSearch();
+        handleBacktoTop();
         handleGoogleMap('google_map');
     });
 })(jQuery);
-var handleSearch = function() {
+var handleSearch = function () {
     $('.search-btn').click(function () {
-        if($('.search-btn').hasClass('show-search-icon')){
-            if ($(window).width()>767) {
+        if ($('.search-btn').hasClass('show-search-icon')) {
+            if ($(window).width() > 767) {
                 $('.search-box').fadeOut(300);
             } else {
                 $('.search-box').fadeOut(0);
             }
             $('.search-btn').removeClass('show-search-icon');
         } else {
-            if ($(window).width()>767) {
+            if ($(window).width() > 767) {
                 $('.search-box').fadeIn(300);
             } else {
                 $('.search-box').fadeIn(0);
@@ -47,12 +48,12 @@ var handleSearch = function() {
     });
 
     // close search box on body click
-    if($('.search-btn').size() != 0) {
-        $('.search-box, .search-btn').on('click', function(e){
+    if ($('.search-btn').size() != 0) {
+        $('.search-box, .search-btn').on('click', function (e) {
             e.stopPropagation();
         });
 
-        $('body').on('click', function() {
+        $('body').on('click', function () {
             if ($('.search-btn').hasClass('show-search-icon')) {
                 $('.search-btn').removeClass("show-search-icon");
                 $('.search-box').fadeOut(300);
@@ -60,17 +61,17 @@ var handleSearch = function() {
         });
     }
 }
-var handleGoogleMap = function(id) {
-    if($('#'+id).length > 0) {
+var handleGoogleMap = function (id) {
+    if ($('#' + id).length > 0) {
         var myMarkers = {
             "markers": [{
                 "latitude": "21.02148",		// latitude
                 "longitude": "105.860529",		// longitude
-                "icon"     : "../img/marker_1.png"	// Pin icon
+                "icon": "../img/marker_1.png"	// Pin icon
             }]
         };
 
-        $("#"+id).mapmarker({
+        $("#" + id).mapmarker({
             zoom: 16,							// Zoom
             center: "21.02148,105.860529",		// Center of map
             type: "ROADMAP",					// Map Type
@@ -92,41 +93,41 @@ var handleGoogleMap = function(id) {
         });
     }
 }
-function autoPlayYouTubeModal(){
+function autoPlayYouTubeModal() {
     var trigger = $("body").find('[data-toggle="modal"]');
-    trigger.click(function() {
-        var theModal = $(this).data( "target" ),
-            videoSRC = $(this).attr( "data-theVideo" ),
-            videoTitle = $(this).data( "title" ),
-            videoSRCauto = videoSRC+"?autoplay=1" ;
+    trigger.click(function () {
+        var theModal = $(this).data("target"),
+            videoSRC = $(this).attr("data-theVideo"),
+            videoTitle = $(this).data("title"),
+            videoSRCauto = videoSRC + "?autoplay=1";
         $(theModal).find('.modal-header>.modal-title').html(videoTitle);
-        $(theModal+' iframe').attr('src', videoSRCauto);
-        $(theModal+' button.close').click(function () {
-            $(theModal+' iframe').attr('src', videoSRC);
+        $(theModal + ' iframe').attr('src', videoSRCauto);
+        $(theModal + ' button.close').click(function () {
+            $(theModal + ' iframe').attr('src', videoSRC);
         });
         $(theModal).on('hidden.bs.modal', function () {
-            $(theModal+' iframe').removeAttr('src');
+            $(theModal + ' iframe').removeAttr('src');
         })
     });
 }
-var hideLocationMap = function(elm) {
-    $('#'+elm).fadeOut();
+var hideLocationMap = function (elm) {
+    $('#' + elm).fadeOut();
 }
-var showLocationMap = function(elm){
+var showLocationMap = function (elm) {
     $('.google_map_location').hide();
-    $('#'+elm).fadeIn();
+    $('#' + elm).fadeIn();
 }
 var marquee = $('p.marquee');
-marquee.each(function() {
-    var mar = $(this),indent = mar.width();
-    mar.marquee = function() {
+marquee.each(function () {
+    var mar = $(this), indent = mar.width();
+    mar.marquee = function () {
         indent--;
-        mar.css('text-indent',indent);
+        mar.css('text-indent', indent);
         if (indent < -1 * mar.children('div.marquee-text').width()) {
             indent = mar.width();
         }
     };
-    mar.data('interval',setInterval(mar.marquee,1000/60));
+    mar.data('interval', setInterval(mar.marquee, 1000 / 60));
 });
 var $animation_elements = $('.animation-element');
 var $window = $(window);
@@ -135,7 +136,7 @@ function check_if_in_view() {
     var window_height = $window.height();
     var window_top_position = $window.scrollTop();
     var window_bottom_position = (window_top_position + window_height);
-    if($animation_elements.length > 0) {
+    if ($animation_elements.length > 0) {
         $.each($animation_elements, function () {
             var $element = $(this);
             var element_height = $element.outerHeight();
@@ -154,3 +155,14 @@ function check_if_in_view() {
 
 $window.on('scroll resize', check_if_in_view);
 $window.trigger('scroll');
+
+var handleBacktoTop = function () {
+    if (($(window).height() + 100) < $(document).height()) {
+        if ($('#top-link-block').length > 0) {
+            $('#top-link-block').removeClass('hidden').affix({
+                // how far to scroll down before link "slides" into view
+                offset: {top: 100}
+            });
+        }
+    }
+}
